7d03eb52ba28e15df78d8e1dddac34bc3daf254a
d4nshields <d4nshields@gmail.com>
2023-03-15 14:46:49 -0400

    fixed imports

--- src/android/PayWithMyBank.java
+++ src/android/PayWithMyBank.java
@@ -1,5 +1,9 @@
 package com.boydgaming.paywithmybank;
 
+import android.content.Intent;
+
+import com.paywithmybank.android.sdk.views.PayWithMyBankView;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -7,22 +11,29 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.logging.Logger;
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class PayWithMyBank extends CordovaPlugin {
     Map<String,String> establishData = new HashMap();
     CallbackContext callInProgress;
+    Logger logger = Logger.getLogger( "PayWithMyBank");
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("selectBankWidget")) {
-            Map<String,String> eData = args.getJSONObject(0).toMap();
-            Iterator keysIter= eData.keys();
+            JSONObject jobj = args.getJSONObject(0);
+            Iterator keysIter= jobj.keys();
             while( keysIter.hasNext()) {
                 String key = (String)keysIter.next();
-                if( null != call.getString( key)) {
-                    this.establishData.put(key, call.getString(key));
+                if( null != jobj.getString( key)) {
+                    this.establishData.put(key, (String)jobj.getString(key));
                 } else {
                     this.establishData.put(key, "");
                 }
@@ -40,33 +51,29 @@ public class PayWithMyBank extends CordovaPlugin {
         Iterator dataIter = this.establishData.keySet().iterator();
         while( dataIter.hasNext()) {
             String key = (String)dataIter.next();
-            Logger.info( "PWMB: selectBankWidget(): "+key+" == "+(String)this.establishData.get( key));
+            logger.info( "PWMB: selectBankWidget(): "+key+" == "+(String)this.establishData.get( key));
         }
 
-        this.getActivity().setContentView( R.layout.layout);
-        PayWithMyBankView trustlyWidget = bridge.getActivity().findViewById( R.id.trustlyWidgetView);
+        this.cordova.getActivity().setContentView( R.layout.layout);
+        PayWithMyBankView trustlyWidget = this.cordova.getActivity().findViewById( R.id.trustlyWidgetView);
         trustlyWidget.selectBankWidget( establishData).onBankSelected( new PayWithMyBankCallback() {
             @Override
             public void handle(Object o, Object o2) {
-                Logger.info( "PWMB: onBankSelected callback()");
-                Logger.info( "PWMB: o="+o.toString());
+                logger.info( "PWMB: onBankSelected callback()");
+                logger.info( "PWMB: o="+o.toString());
 
                 if( o2 instanceof HashMap) {
                     HashMap data = (HashMap)o2;
                     String paymentProviderId = (String)data.get( "paymentProviderId");
-                    Logger.info( "PWMB: paymentProviderId = "+paymentProviderId);
+                    logger.info( "PWMB: paymentProviderId = "+paymentProviderId);
                     establishData.put( "paymentProviderId", paymentProviderId);
                 }
 
-                Intent intent = new Intent( bridge.getActivity(), LightboxActivity.class);
+                Intent intent = new Intent( cordova.getActivity(), LightboxActivity.class);
                 intent.putExtra(LightboxActivity.ESTABLISH_DATA, (Serializable) establishData);
-                bridge.getActivity().startActivity(intent);
+                cordova.getActivity().startActivity(intent);
 
-                try {
-                    call.success( new JSONObject( establishData));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                callInProgress.success( new JSONObject( establishData));
             }
         });
 
