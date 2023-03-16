ca552a41625c2bb031c863cbc4d815e136913c2b
d4nshields <d4nshields@gmail.com>
2023-03-16 16:11:40 -0400

    refactoring PayWithMyBank so that it has an Activity wraped around it.

--- src/android/PayWithMyBank.java
+++ src/android/PayWithMyBank.java
@@ -34,7 +34,7 @@ public class PayWithMyBank extends CordovaPlugin {
     Map<String,String> establishData = new HashMap();
     CallbackContext callInProgress;
     Logger logger = Logger.getLogger( "PayWithMyBank");
-    ActivityResultLauncher<Intent> mStartForResult;
+    ActivityResultLauncher<Intent> mStartSelectBankForResult;
     protected PayWithMyBankView trustlyWidget;
     protected Resources resources;
     protected Application app;
@@ -43,12 +43,11 @@ public class PayWithMyBank extends CordovaPlugin {
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize( cordova, webView);
-        mStartForResult = cordova.getActivity().registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        mStartSelectBankForResult = cordova.getActivity().registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             @Override
             public void onActivityResult( ActivityResult result) {
-                logger.info( "PWMB: PayWithMyBank...onActivityResult(): ");
-                trustlyWidget.removeAllViewsInLayout();
-                //trustlyWidget.destroy();
+                logger.info( "PWMB: PayWithMyBank...SelectBank...onActivityResult(): ");
+
                 callInProgress.success( new JSONObject( establishData));
             }
         });
@@ -87,29 +86,9 @@ public class PayWithMyBank extends CordovaPlugin {
             logger.info( "PWMB: selectBankWidget(): "+key+" == "+(String)this.establishData.get( key));
         }
 
-        int layout_id = resources.getIdentifier( "layout", "layout", package_name);
-        logger.info( "PWMB: layout_id == "+layout_id);
-        int trustly_widget_view_id = resources.getIdentifier( "trustlyWidgetView", "id", package_name);
-        logger.info( "PWMB: trustly_widget_view_id == "+trustly_widget_view_id);
+        Intent intent = new Intent( this, LightboxActivity.class);
+        intent.putExtra(LightboxActivity.ESTABLISH_DATA, (Serializable) establishData);
+        mStartSelectBankForResult.launch( intent);
 
-        this.cordova.getActivity().setContentView( layout_id);
-        trustlyWidget = this.cordova.getActivity().findViewById( trustly_widget_view_id);
-        trustlyWidget.selectBankWidget( establishData).onBankSelected( new PayWithMyBankCallback<com.paywithmybank.android.sdk.interfaces.PayWithMyBank, Map<String, String>>() {
-            @Override
-            public void handle(com.paywithmybank.android.sdk.interfaces.PayWithMyBank o, Map<String, String> o2) {
-                logger.info( "PWMB: onBankSelected callback()");
-
-                if( o2 instanceof HashMap) {
-                    HashMap data = (HashMap)o2;
-                    String paymentProviderId = (String)data.get( "paymentProviderId");
-                    logger.info( "PWMB: paymentProviderId = "+paymentProviderId);
-                    establishData.put( "paymentProviderId", paymentProviderId);
-                }
-
-                Intent intent = new Intent( cordova.getActivity(), LightboxActivity.class);
-                intent.putExtra(LightboxActivity.ESTABLISH_DATA, (Serializable) establishData);
-                mStartForResult.launch( intent);
-            }
-        });
     }
 }
---
+++ src/android/PayWithMyBankActivity.java
@@ -0,0 +1,101 @@
+package com.boydgaming.paywithmybank;
+
+import android.content.Intent;
+import android.app.Application;
+import android.content.res.Resources;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+
+import com.paywithmybank.android.sdk.interfaces.PayWithMyBankCallback;
+import com.paywithmybank.android.sdk.views.PayWithMyBankView;
+
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class PayWithMyBank extends AppCompatActivity {
+    Map<String,String> establishData = new HashMap();
+    Logger logger = Logger.getLogger( "PayWithMyBankActivity");
+    ActivityResultLauncher<Intent> mStartLightboxForResult;
+    protected PayWithMyBankView trustlyWidget;
+    protected Resources resources;
+    protected Application app;
+    protected String package_name;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState)
+    {
+        AppCompatActivity myInstance = this;
+        super.onCreate( savedInstanceState);
+
+        mStartLightboxForResult = this.registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult( ActivityResult result) {
+                logger.info( "PWMB: PayWithMyBank...onActivityResult(): ");
+                trustlyWidget.removeAllViewsInLayout();
+                //trustlyWidget.destroy();
+                this.finish();
+            }
+        });
+
+        Intent intent = getIntent();
+        this.establishData = (Map<String,String>)intent.getSerializableExtra( ESTABLISH_DATA);
+
+        this.selectBankWidget();
+        return true;
+    }
+
+    private void selectBankWidget() {
+        app=this.getApplication();
+        package_name = app.getPackageName();
+        resources = app.getResources();
+
+        Iterator dataIter = this.establishData.keySet().iterator();
+        while( dataIter.hasNext()) {
+            String key = (String)dataIter.next();
+            logger.info( "PWMB: selectBankWidget(): "+key+" == "+(String)this.establishData.get( key));
+        }
+
+        int layout_id = resources.getIdentifier( "layout", "layout", package_name);
+        logger.info( "PWMB: layout_id == "+layout_id);
+        int trustly_widget_view_id = resources.getIdentifier( "trustlyWidgetView", "id", package_name);
+        logger.info( "PWMB: trustly_widget_view_id == "+trustly_widget_view_id);
+
+        this.setContentView( layout_id);
+        trustlyWidget = this.findViewById( trustly_widget_view_id);
+        trustlyWidget.selectBankWidget( establishData).onBankSelected( new PayWithMyBankCallback<com.paywithmybank.android.sdk.interfaces.PayWithMyBank, Map<String, String>>() {
+            @Override
+            public void handle(com.paywithmybank.android.sdk.interfaces.PayWithMyBank o, Map<String, String> o2) {
+                logger.info( "PWMB: onBankSelected callback()");
+
+                if( o2 instanceof HashMap) {
+                    HashMap data = (HashMap)o2;
+                    String paymentProviderId = (String)data.get( "paymentProviderId");
+                    logger.info( "PWMB: paymentProviderId = "+paymentProviderId);
+                    establishData.put( "paymentProviderId", paymentProviderId);
+                }
+
+                Intent intent = new Intent( this, LightboxActivity.class);
+                intent.putExtra(LightboxActivity.ESTABLISH_DATA, (Serializable) establishData);
+                mStartLightboxForResult.launch( intent);
+            }
+        });
+    }
+}
